a1e1ff2409a80daa918152488c268656e37be464
Brijesh Kumar <bk10895@gmail.com>
2024-06-26 20:42:47 +0530

    feat: add Header, Footer, and Loader components
    - Add Header, Footer, and Loader components to improve user navigation and feedback
    - Update the layout to include the new Header and Footer components
    - Add suspense fallback to the layout for better loading experience
    - Refactor Home component for better structure
    - Update tailwind configuration to include new color variants

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,12 @@ import {
   SignedOut,
   UserButton,
 } from '@clerk/nextjs';
+import { Footer, Header } from '@/components';
 
 import { Inter } from 'next/font/google';
+import Loading from './loading';
 import type { Metadata } from 'next';
+import { Suspense } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -27,14 +30,19 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang='en'>
-        <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          {children}
+        <body className={inter.className}>
+          <Header />
+          <Suspense fallback={<Loading />}>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+            <main>{children}</main>
+          </Suspense>
+          <SignedOut></SignedOut>
+          <Footer />
         </body>
       </html>
     </ClerkProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <>
+    <div className='flex flex-col mx-auto justify-center items-center w-full'>
       <p>Recipe Master</p>
-    </>
-
+    </div>
   );
 }
