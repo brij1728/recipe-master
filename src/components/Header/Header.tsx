9fc9ca095a91de0956837f2e67345e717773ac94
Brijesh Kumar <bk10895@gmail.com>
2024-06-26 22:18:20 +0530

    feat: enhance header component and update color scheme in Tailwind config
    - Add styled header component with navigation link to homepage
    - Update color scheme in Tailwind configuration for primary and secondary colors
    - Header now includes brand title 'Foodie - The Recipe Book'

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,17 @@
+import Link from 'next/link';
 import React from 'react';
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className='bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 shadow-md py-4'>
+      <div className='md:container md:mx-auto flex justify-between items-center px-4'>
+        <Link
+          className='sm:text-3xl text-base sm:font-bold font-normal text-white'
+          href='/'
+        >
+          Foodie - The Recipe Book
+        </Link>
+      </div>
+    </header>
+  );
 };
