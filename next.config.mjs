5ed38a87fc9535a1bf5c466b497ee3469489f962
Brijesh Kumar <bk10895@gmail.com>
2024-06-27 22:45:56 +0530

    chore: Update next.config.mjs with CLERK_PUBLISHABLE_KEY environment variable

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+  },
+};
 
 export default nextConfig;
