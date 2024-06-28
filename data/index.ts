08e7ed60c73f562d24f4291e9af8e604f8b419b5
Brijesh Kumar <bk10895@gmail.com>
2024-06-28 14:52:55 +0530

    feat: implement dynamic navigation menu and add react-icons package
    - Add dynamic navigation menu functionality to the header component
    - Update 'Header.tsx' to include state for menu open/close
    - Add 'NavMenu.tsx' for reusable navigation menu component
    - Update 'index.ts' in the 'ui' directory to export 'NavMenu'
    - Add 'index.ts' in the 'data' directory to serve as a source for menu items
    - Introduce 'react-icons' package for menu open/close icons

---
+++ data/index.ts
@@ -0,0 +1,18 @@
+export const MenuItems = [
+  {
+    name: 'Home',
+    link: '/',
+  },
+  {
+    name: 'Recipes',
+    link: '/recipes',
+  },
+  {
+    name: 'Categories',
+    link: '/categories',
+  },
+  {
+    name: 'About',
+    link: '/about',
+  },
+];
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,19 @@
+'use client';
+
+import { FaBars, FaTimes } from 'react-icons/fa';
+import React, { useState } from 'react';
+
 import Link from 'next/link';
-import React from 'react';
+import { MenuItems } from '../../../data';
+import { NavMenu } from '../ui';
 
 export const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className='bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 shadow-md py-4'>
       <div className='md:container md:mx-auto flex justify-between items-center px-4'>
@@ -11,7 +23,36 @@ export const Header = () => {
         >
           Foodie - The Recipe Book
         </Link>
+        <button className='text-white text-2xl md:hidden' onClick={toggleMenu}>
+          {menuOpen ? <FaTimes /> : <FaBars />}
+        </button>
+        <NavMenu
+          menus={MenuItems}
+          navClassName='hidden md:block'
+          ulClassName='flex flex-col md:flex-row'
+          liClassName='my-2 md:my-0'
+          linkClassName='text-white text-lg md:text-base hover:underline'
+        />
       </div>
+      {menuOpen && (
+        <div className='fixed inset-0 bg-black bg-opacity-50 flex justify-end items-start z-50'>
+          <div className='bg-white rounded-lg p-2 m-4 relative w-40 shadow-lg'>
+            <button
+              className='text-black text-2xl absolute top-2 right-2'
+              onClick={toggleMenu}
+            >
+              <FaTimes />
+            </button>
+            <NavMenu
+              menus={MenuItems}
+              navClassName='block md:hidden'
+              ulClassName='flex flex-col items-center'
+              liClassName='my-1'
+              linkClassName='text-black text-lg hover:underline'
+            />
+          </div>
+        </div>
+      )}
     </header>
   );
 };
