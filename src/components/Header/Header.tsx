0735e2b6cd76d26d5643ca08d146b192d457cc5d
Brijesh Kumar <bk10895@gmail.com>
2024-06-28 21:52:38 +0530

    refactor: improve UI layout and functionality in Header component
    - Remove condition to hide header during sign-in process
    - Restructure layout to improve UI/UX, especially on mobile view
    - Move Sign In and UserButton to navigation menu for better accessibility
    - Adjust styles and transitions to enhance visual feedback

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -28,11 +28,6 @@ export const Header = () => {
     openSignIn({ afterSignInUrl: '/' });
   };
 
-  // Hide header during sign-in process
-  if (signInModalOpen) {
-    return null;
-  }
-
   return (
     <header className='bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 shadow-md py-4'>
       <div className='md:container md:mx-auto flex justify-between items-center px-4'>
@@ -42,28 +37,40 @@ export const Header = () => {
         >
           Foodie - The Recipe Book
         </Link>
-        <button className='text-white text-2xl md:hidden' onClick={toggleMenu}>
-          {menuOpen ? <FaTimes /> : <FaBars />}
-        </button>
-        <NavMenu
-          menus={MenuItems}
-          navClassName='hidden md:block'
-          ulClassName='flex flex-col md:flex-row'
-          liClassName='my-2 md:my-0'
-          linkClassName='text-white text-lg md:text-base hover:text-secondary-100 transition-colors duration-300 md:mx-4'
-        />
-        <SignedOut>
-          <button className='items-center text-white' onClick={handleSignIn}>
-            Sign In
+        <div className='flex items-center'>
+          <button
+            className='text-white text-2xl md:hidden'
+            onClick={toggleMenu}
+          >
+            {menuOpen ? <FaTimes /> : <FaBars />}
           </button>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
+          <div className='hidden md:flex items-center'>
+            <NavMenu
+              menus={MenuItems}
+              navClassName='md:flex'
+              ulClassName='flex flex-col md:flex-row'
+              liClassName='my-2 md:my-0'
+              linkClassName='text-white text-lg md:text-base hover:text-secondary-100 transition-colors duration-300 md:mx-4'
+            />
+            <SignedOut>
+              <button
+                className='ml-4 items-center text-white'
+                onClick={handleSignIn}
+              >
+                Sign In
+              </button>
+            </SignedOut>
+            <SignedIn>
+              <div className='ml-4 items-center text-white'>
+                <UserButton />
+              </div>
+            </SignedIn>
+          </div>
+        </div>
       </div>
       {menuOpen && (
-        <div className='fixed inset-0 bg-black bg-opacity-50 flex justify-end items-start z-50'>
-          <div className='bg-white rounded-lg p-2 m-4 relative w-40 shadow-lg'>
+        <div className='fixed inset-0 bg-black bg-opacity-50 flex flex-col justify-start items-center z-50'>
+          <div className='bg-white rounded-lg p-4 m-4 relative w-64 shadow-lg'>
             <button
               className='text-black text-2xl absolute top-2 right-2'
               onClick={toggleMenu}
@@ -73,10 +80,25 @@ export const Header = () => {
             <NavMenu
               menus={MenuItems}
               navClassName='block md:hidden'
-              ulClassName='flex flex-col items-start p-4'
-              liClassName='my-1'
+              ulClassName='flex flex-col items-start'
+              liClassName='my-2'
               linkClassName='text-black text-lg hover:text-gray-500 transition-colors duration-300'
             />
+            <SignedOut>
+              <button
+                // className='mt-4 w-full bg-blue-500 text-white py-2 rounded'
+                onClick={handleSignIn}
+              >
+                Sign In
+              </button>
+            </SignedOut>
+            <SignedIn>
+              <div
+              //  className='mt-4 w-full bg-blue-500 text-white py-2 rounded'
+              >
+                <UserButton />
+              </div>
+            </SignedIn>
           </div>
         </div>
       )}
