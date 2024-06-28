2f5e6ba8011000af9c1806f53c40f51e443a2b1b
Brijesh Kumar <bk10895@gmail.com>
2024-06-28 20:41:06 +0530

    feat: enhance website layout and update footer
    - Modify global CSS to introduce flex layout for better structure
    - Update Footer component with gradient background and footer text

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className='bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 shadow-md py-2 sm:py-4 w-full fixed bottom-0'>
+      <div className='container mx-auto text-center'>
+        <p className='sm:text-base text-xs'>
+          © 2024 Foodie - The Recipe Book. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
 };
